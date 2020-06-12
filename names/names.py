64c06ea0a567beb693f65e79b3b0d4780335f67e
unknown <heedu40@gmail.comgit commit -m Finishes>
2020-06-12 12:03:03 -0400

    Finishes requirements

--- names/names.py
+++ names/names.py
@@ -13,10 +13,60 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
+
+class BSTNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        if value < self.value:
+            if not self.left:
+                self.left = BSTNode(value)
+            else:
+                self.left.insert(value)
+        else:
+            if not self.right:
+                self.right = BSTNode(value)
+            else:
+                self.right.insert(value)
+
+    def dupes(self, target):
+        if target == self.value:
+            return duplicates.append(target)
+        if target < self.value:
+            if not self.left:
+                return False
+            return self.left.dupes(target)
+        else:
+            if not self.right:
+                return False
+            return self.right.dupes(target)
+
 for name_1 in names_1:
     for name_2 in names_2:
         if name_1 == name_2:
             duplicates.append(name_1)
+
+end_time = time.time()
+print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print (f"runtime: {end_time - start_time} seconds")
+
+
+start_time = time.time()
+
+
+duplicates = []
+
+BST = BSTNode(names_1[0])
+
+for name_1 in names_1[1:]:
+    BST.insert(name_1)
+
+for name_2 in names_2:
+    BST.dupes(name_2)
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
